7a253d5230181c6b54777df411697bd9403d8cfc
Hoang <tanhbinhkhtc@gmail.com>
2024-07-03 10:28:17 +0700

    +Map2 prefab

--- Assets/Scripts/BounceObj.cs
+++ Assets/Scripts/BounceObj.cs
@@ -17,14 +17,18 @@ public class BounceObj : MonoBehaviour
     public Vector3 GetDirectionBounce(Vector3 towardDirection)
     {
         Vector3 result = Vector3.zero;
+        float rever = 1f;
         animator.SetTrigger(bounceAnim);
-        if (towardDirection.x > towardDirection.z)
+        if (Math.Abs(towardDirection.x) > Math.Abs(towardDirection.z))
         {
-            result = new Vector3(0, 0, towardDirection.x * bouncePos.y * -1);
+            if (towardDirection.x > 0) rever = -1f;
+            result = new Vector3(0, 0, towardDirection.x * bouncePos.y * rever);
             return result;
         }
 
-        result = new Vector3(towardDirection.z * bouncePos.x * -1, 0, 0f);
+        if (towardDirection.z > 0) rever = -1f;
+        result = new Vector3(towardDirection.z * bouncePos.x*rever, 0, 0f);
+        //Debug.Log(result);
         return result;
     }
 }
